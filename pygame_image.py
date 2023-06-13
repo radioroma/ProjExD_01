bff7ab79c08e010b3f64f9b3832e070776d10817
Sunager <c0b220829c@deu.teu.ac.jp>
2023-06-13 16:37:40 +0900

    練習5

--- pygame_image.py
+++ pygame_image.py
@@ -7,17 +7,20 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
-    kk_img =pg.image.load("ex01/fig/pg_bg.jpg")
-    kk_imgs=[kk_img,
-             pg.transform(kk_img,10,1.0)
-             ]
-    dmr = 0
+    kk_img =pg.image.load("ex01/fig/3.png")
+    kk_img=pg.transform.flip(kk_img,True,False)
+    kt_img=pg.transform.rotozoom(kk_img,10,1.0)
+    kk_imgs=[ pg.transform.flip(pg.image.load("ex01/fig/3.png"),True,False),
+        pg.transform.rotozoom(kk_img,10,1.0)
+    ]
+          
+    tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        #screen.blit(bg_img,[300,200])
+        screen.blit(kk_imgs[tmr%2],[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
