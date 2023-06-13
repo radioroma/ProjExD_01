c4a871d94b301e5cee51b1c7c757394e727e3d33
Sunager <c0b220829c@deu.teu.ac.jp>
2023-06-13 15:54:59 +0900

    練習1

--- pygame_image.py
+++ pygame_image.py
@@ -1,22 +1,18 @@
 import sys
 import pygame as pg
 
+
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
-    kk_img = pg.image.load("ex01/fig/3.png")
-    kk_img= pg.transform.rotozoom(kk_img,10,1.0)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [300, 200])
-        
-
         pg.display.update()
         tmr += 1        
         clock.tick(10)
