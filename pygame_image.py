3ce6e05a124d9c5dcebe1631fce658b71e8cd034
Sunager <c0b220829c@deu.teu.ac.jp>
2023-06-13 17:31:57 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -1,9 +1,10 @@
 import sys
 import pygame as pg
 bg_x =0
-
+kk_x =0
+kk_y=0
 def main():
-    global bg_x
+    global bg_x,kk_x,kk_y
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
@@ -14,19 +15,22 @@ def main():
     kk_imgs=[ pg.transform.flip(pg.image.load("ex01/fig/3.png"),True,False),
         pg.transform.rotozoom(kk_img,10,1.0)
     ]
-          
     tmr = 0
+    tmr2 =0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+            if 0-bg_x ==1400:
+                bg_x = 0
         bg_x = (bg_x+4)%1600
-        screen.blit(bg_img, [bg_x+0, 0])
-        screen.blit(kk_imgs[tmr%2],[300,200])
+
+        screen.blit(bg_img, [0-bg_x, 0])
+        screen.blit(kk_imgs[tmr%2],[300,+200])
         # bg_x = (bg_x+4)%1600
         #screen.blit(bg_img, [bg_x-1600, 0])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(500)
 
 
 if __name__ == "__main__":
